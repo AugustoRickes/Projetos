2000a0416539a672107419ff253cc24a58da6068
Augusto Rickes da Silveira <ag.augusto.rickes@gmail.com>
2022-06-03 16:10:51 -0300

    sucrase ok

--- SISTEMADEAGENDAMENTO/src/router.js
+++ SISTEMADEAGENDAMENTO/src/router.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-return res.json({message: 'okay'})
+return res.json({message: 'ok'})
 })
 
 module.exports = routes;
